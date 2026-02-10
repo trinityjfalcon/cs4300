3c1bb7da1db69605d29daad3e0d20551c256c6a4
trinityjfalcon <trinity.falcon1@gmail.com>
2026-02-10 06:29:53 +0000

    test failed and after editing test it passed!!!

--- homework1/tests/test_task1.py
+++ homework1/tests/test_task1.py
@@ -1,4 +1,9 @@
-def test_task1_output():
+import sys
+import os
+
+sys.path.append(os.path.abspath("src"))
+
+def test_task1_output(capsys):
     import task1
     captured = capsys.readouterr()
     assert captured.out == "Hello World!\n"
