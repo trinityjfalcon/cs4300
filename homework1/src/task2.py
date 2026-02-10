7272863ddbcc57da3afe2822838e6a624bf37078
trinityjfalcon <trinity.falcon1@gmail.com>
2026-02-10 06:42:28 +0000

    created task2 py file

---
+++ homework1/src/task2.py
@@ -0,0 +1,16 @@
+#Create a new file named task2.py demonstrating the use of various data types, including integers,
+#floating-point numbers, strings, and boolean. Implement pytest to test case for each data type,
+#ensuring that the script’s behavior matches the expected outcomes
+
+intType = 5
+print("\nThis is an integer:", intType)
+
+floatType = 3.5
+print("\nThis is a floating point number:", floatType)
+
+stringType = "penguin"
+print("\nThis is a string:", stringType)
+
+boolType = True
+print("\nThis is the boolean value:", boolType)
+print("\nThis is negating the boolean value:", not boolType)
