d857f04876644c7fba6c48677c2c484479563fd0
trinityjfalcon <trinity.falcon1@gmail.com>
2026-02-11 07:24:22 +0000

    Completed task5

---
+++ homework1/src/task5.py
@@ -0,0 +1,17 @@
+#Create a new file named task5.py and inside create a list of your favorite books, including book
+#titles and authors. Use list slicing to print the first three books in the list. Create a dictionary that
+#represents a basic student database, including student names and their corresponding student IDs.
+
+# List of favorite books
+bookList = ["The Poppy War by R.F. Kuang", "The Dragon Republic by R.F. Kuang", "Before the Coffee Gets Cold by Toshikazu Kawaguchi", "Before We Say Goodbye by Toshikazu Kawaguchi", "Vicious by V.E. Schwab", "Heartless by Marissa Meyer"]
+print(bookList[0:3])
+
+# Dictionary
+studentDict = {
+    "Shoobert Magoo": 101
+    "Bobert McGee": 102
+    "Muffin Man": 103
+    "Jimbob Borito": 104
+    "Snoopy Dawg": 105
+    "Abo Rito": 106
+}
