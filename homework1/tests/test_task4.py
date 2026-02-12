ef2055802f5a6cbd88a8fc980601afee7b25e504
trinityjfalcon <trinity.falcon1@gmail.com>
2026-02-12 07:31:14 +0000

    successful test_task4

--- homework1/tests/test_task4.py
+++ homework1/tests/test_task4.py
@@ -1,12 +1,13 @@
+import pytest
 from task4 import calculate_discount
 
 def test_integer():
     assert calculate_discount(100, 5) == 95
 
 def test_float():
-    assert calculate_discount(20.50, 20) == 16.4
-    assert calculate_discount(100, 10.5) == 89.5
-    assert calculate_discount(25.5, 45.5) == 13.8975
+    assert calculate_discount(20.50, 20) == pytest.approx(16.4, 0.01)
+    assert calculate_discount(100, 10.5) == pytest.approx(89.5, 0.01)
+    assert calculate_discount(25.5, 45.5) == pytest.approx(13.8975, 0.01)
 
 
 def test_zero():
