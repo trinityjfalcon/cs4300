320e05ea2e17918e5bdd7f33469bd52faef6bf39
trinityjfalcon <trinity.falcon1@gmail.com>
2026-02-22 08:07:03 +0000

    Created serializers for bookings

---
+++ homework2/bookings/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Movie, Seat, Booking
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class SeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seat
+        fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
--- homework2/bookings/views.py
+++ homework2/bookings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 from .models import Movie, Seat, Booking
+from rest_framework import viewsets
 
-# Create your views here.
+class MovieViewSet()
