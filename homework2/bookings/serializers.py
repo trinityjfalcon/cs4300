1b16e35a4d914dd73b0d7369a3a00065ce6917ed
trinityjfalcon <trinity.falcon1@gmail.com>
2026-02-23 21:51:23 +0000

    Completed viewsets

--- homework2/bookings/serializers.py
+++ homework2/bookings/serializers.py
@@ -1,17 +1,34 @@
 from rest_framework import serializers
 from .models import Movie, Seat, Booking
 
+"""
+Serializers allow complex data like querysets and model instances 
+to be converted to native Python datatypes and can be easily rendered
+into JSON or other content types. Also handles deserialization.
+"""
+
 class MovieSerializer(serializers.ModelSerializer):
+    """
+    Uses ModelSerializer to automatically generate fields based on the
+    Movie model definition
+    """
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = '__all__'     #include all model fields in API
 
 class SeatSerializer(serializers.ModelSerializer):
+    """
+    Converts Seat model instances to/from JSON format for API communication
+    """
     class Meta:
         model = Seat
-        fields = '__all__'
+        fields = '__all__'   # Expose all seat fields
 
 class BookingSerializer(serializers.ModelSerializer):
+    """
+    Handles validation and transformation of Booking data between JSON and
+    Django model instances
+    """
     class Meta:
         model = Booking
         fields = '__all__'
--- homework2/bookings/views.py
+++ homework2/bookings/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from .models import Movie, Seat, Booking
 from rest_framework import viewsets
+from .serializers import MovieSerializers, SeatSerializer, BookingSerializer
 
-class MovieViewSet()
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = SeatSerializer
+
+class BookingViewSet(viewsets.MovieViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = BookingSerializer
