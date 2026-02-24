22af866dd9fe8651a3ca42d849d1927daed45484
trinityjfalcon <trinity.falcon1@gmail.com>
2026-02-24 06:49:27 +0000

    Created a templates directory and created a few html files. Started 3.4

--- homework2/bookings/views.py
+++ homework2/bookings/views.py
@@ -3,14 +3,19 @@ from .models import Movie, Seat, Booking
 from rest_framework import viewsets
 from .serializers import MovieSerializers, SeatSerializer, BookingSerializer
 
+"""
+ ModelViewSet automatically provides list(), retrieve(), create(), update(),
+ partial_update(), and destroy()
+"""
+
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 class SeatViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
+    queryset = Seat.objects.all()
     serializer_class = SeatSerializer
 
 class BookingViewSet(viewsets.MovieViewSet):
-    queryset = Movie.objects.all()
+    queryset = Booking.objects.all()
     serializer_class = BookingSerializer
