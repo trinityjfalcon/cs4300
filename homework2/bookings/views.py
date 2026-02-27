76ad2c6fac76888995bebc13f573c47585d36168
trinityjfalcon <trinity.falcon1@gmail.com>
2026-02-27 09:21:24 +0000

    Seats can be booked

--- homework2/bookings/views.py
+++ homework2/bookings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Movie, Seat, Booking
 from rest_framework import viewsets
 from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
@@ -28,4 +28,24 @@ def movie_list(request):
 def book_seat(request, movie_id):
     movie = get_object_or_404(Movie, id=movie_id)
     seats = Seat.objects.all()
+
+    if request.method == "POST":
+        seat_id = request.POST.get("seat_id")
+        seat = get_object_or_404(Seat, id=seat_id)
+
+        if seat.booking_status == Seat.BookingStatus.AVAILABLE:
+            #Mark seat as unavailable
+            seat.booking_status = Seat.BookingStatus.UNAVAILABLE
+            seat.save()
+
+            #Create the booking
+            Booking.objects.create(
+                movie=movie,
+                seat=seat,
+                user=request.user,
+                booking_date=None
+            )
+
+        return redirect("book_seat", movie_id=movie_id)
+
     return render(request, "bookings/seat_booking.html", {"movie": movie, "seats": seats})
