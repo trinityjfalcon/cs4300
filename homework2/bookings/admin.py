085ca7a5c990a78f24ce8e1c2e225355f792fec7
trinityjfalcon <trinity.falcon1@gmail.com>
2026-02-27 07:58:34 +0000

    Fixed more bugs and added admin to add movies

--- homework2/bookings/admin.py
+++ homework2/bookings/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Movie, Seat, Booking
 
-# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Seat)
+admin.site.register(Booking)
