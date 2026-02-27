8772077939e2c38dde392e268d67b8801b5406b7
trinityjfalcon <trinity.falcon1@gmail.com>
2026-02-27 04:02:25 +0000

    Fixing syntax and debugging

--- homework2/bookings/views.py
+++ homework2/bookings/views.py
@@ -7,7 +7,7 @@ from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
  ModelViewSet automatically provides list(), retrieve(), create(), update(),
  partial_update(), and destroy()
 """
-
+# ViewSets for APIs
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
@@ -16,6 +16,16 @@ class SeatViewSet(viewsets.ModelViewSet):
     queryset = Seat.objects.all()
     serializer_class = SeatSerializer
 
-class BookingViewSet(viewsets.MovieViewSet):
+class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+# Views for templates
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, "bookings/movie_list.html", {"movies": Movie})
+
+def book_seat(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    seats = Seat.objects.all()
+    return render(request, "bookings/movie_list.html", {"movies": Movie, "seats": seats})
