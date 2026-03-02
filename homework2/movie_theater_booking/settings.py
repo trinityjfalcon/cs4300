21220f34f4ed5cadbfaef0e44c4295a341ff5ec9
trinityjfalcon <trinity.falcon1@gmail.com>
2026-03-02 20:41:47 +0000

    setup for deploy to render (not totally working)

--- homework2/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,15 +80,9 @@ WSGI_APPLICATION = 'movie_theater_booking.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    """
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    """
     'default': dj_database_url.config(
         default='postgresql://postgres:postgres@localhost:5432/movie_theater_booking',
-        conn_max_age=600,
+        conn_max_age=600
     )
 }
 
