b8f0a4b42376ba7a0acf602e08c7a35d1d8023ee
trinityjfalcon <trinity.falcon1@gmail.com>
2026-03-02 19:51:13 +0000

    behave test for last scenario

--- homework2/bookings/features/steps/booking_steps.py
+++ homework2/bookings/features/steps/booking_steps.py
@@ -48,3 +48,29 @@ def step_seat_unavailable(context):
 @then('a booking should be created')
 def step_booking_created(context):
     assert Booking.objects.filter(user=context.user).count() == 1
+
+@given('I have an existing booking')
+def step_booking_exists(context):
+    context.movie = Movie.objects.create(
+        title='Booking BDD Movie',
+        description='Booking BDD test',
+        release_date=datetime.date(2000, 1, 1),
+        duration=101
+    )
+    context.seat = Seat.objects.create(
+        movie=context.movie,
+        seat_num=1
+    )
+    Booking.objects.create(
+        movie=context.movie,
+        seat=context.seat,
+        user=context.user
+    )
+
+@when('I visit the booking history page')
+def step_visit_history(context):
+    context.response = context.client.get('/history/')
+
+@then('I should see my booking')
+def step_see_booking(context):
+    assert context.response.status_code == 200
