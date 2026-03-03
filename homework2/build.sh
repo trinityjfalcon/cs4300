a2e822cf9bf95e515a61e917a0e05e3a1d227ece
trinityjfalcon <trinity.falcon1@gmail.com>
2026-03-03 01:46:19 +0000

    create superuser attempt

--- homework2/build.sh
+++ homework2/build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+echo "from django.contrib.auth.models import User; User.objects.filter(username='tfalcon').exists() or User.objects.create_superuser('tfalcon', 'admin@example.com', 'PenwinPenguin1')" | python manage.py shell
